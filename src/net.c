2b688a6420cc22e265994bdf68a952d3dad5b58e
Christian Spielberger <c.spielberger@commend.com>
2019-09-16 13:49:21 +0200

    net: add pre proc switch NODNS

--- src/net.c
+++ src/net.c
@@ -355,11 +355,13 @@ int net_alloc(struct network **netp, const struct config_net *cfg)
 	}
 
 	/* Initialise DNS resolver */
+#ifndef NODNS
 	err = dns_init(net);
 	if (err) {
 		warning("net: dns_init: %m\n", err);
 		goto out;
 	}
+#endif
 
 	sa_init(&net->laddr, AF_INET);
 
